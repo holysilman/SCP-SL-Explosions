d6570c677440124b23fc97f4ced082b94ca681f7
holysilman <129124808+holysilman@users.noreply.github.com>
2023-03-27 19:19:44 -0400

    Add files via upload

---
+++ Explode/Explode/Configs/Config.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Explode
+{
+    public class Config
+    {
+        [Description("Chance to explode while sprinting")]
+        public float explodeChance { get; set; } = 0.1f;
+    }
+}
---
+++ Explode/Explode/EventHandlers.cs
@@ -0,0 +1,30 @@
+﻿namespace Explode
+{
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+    using PluginAPI.Core;
+    using PluginAPI.Core.Items;
+    using UnityEngine;
+    using Footprinting;
+    using InventorySystem;
+    using InventorySystem.Items;
+    using InventorySystem.Items.ThrowableProjectiles;
+
+    public class EventHandlers
+    {
+        [PluginEvent(ServerEventType.PlayerPreCoinFlip)]
+        public bool OnPlayerPreCoinFlip(Player player)
+        {
+            float randNext = (float)Plugin.Random.NextDouble();
+            Log.Info(randNext.ToString());
+            if (randNext <= Plugin.Instance.Config.explodeChance)
+            {
+                InventoryItemLoader.AvailableItems.TryGetValue(ItemType.GrenadeHE, out ItemBase itemBase);
+                ThrowableItem throwableItem = (ThrowableItem)itemBase;
+                ExplosionGrenade.Explode(new Footprint(player.ReferenceHub), player.Position, (ExplosionGrenade)throwableItem.Projectile);
+                return false;
+            }
+            return true;
+        }
+    }
+}
