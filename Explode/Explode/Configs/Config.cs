bf195cc5b8e34c8b10975574412bbd9c8366c8d1
holysilman <129124808+holysilman@users.noreply.github.com>
2023-03-28 14:58:47 -0400

    Added new effects given to the player when flipped
    Heads = positive effects, tails = negative effects

--- Explode/Explode/Configs/Config.cs
+++ Explode/Explode/Configs/Config.cs
@@ -1,10 +1,64 @@
 ﻿using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace Explode
 {
     public class Config
     {
         [Description("Chance to explode while sprinting")]
-        public float explodeChance { get; set; } = 0.1f;
+        public float ExplodeChance { get; set; } = 0.1f;
+
+        [Description("Toggle to enable or disable giving effects/items for flipping coin")]
+        public bool HasEffects { get; set; } = true;
+        [Description("Toggle to remove coin after being flipped")]
+        public bool LoseCoin { get; set; } = true;
+        [Description("Duration of time-based effects")]
+        public int Duration { get; set; } = 10;
+
+        [Description("Number of good effects. Only modify to be less than or equal to 10")]
+        public int NumGoodEffects { get; set; } = 10;
+
+        [Description("Number of badd effects. Only modify to be less than or equal to 20")]
+        public int NumBadEffects { get; set; } = 20;
+
+        [Description("Decimal representing percent chance to have the extreme effects occur. Will not completely make it 100% if set to 1")]
+        public float PercentExtremes { get; set; } = 0.01f;
+
+        [Description("Integer equivalent to SCP:SL effects, Do not modify")]
+        public enum CoinEffects
+        {
+            BodyshotReduction = 0,
+            DamageReduction,
+            Invigorated,
+            Invisibility,
+            MovementBoost,
+            SCP1853,
+            SpawnProtection,
+            Rainbow,
+            Vitality,
+            SCP207,
+            HotDamn,
+            Amnesia,
+            Asphyxiated,
+            Bleeding,
+            Blinded,
+            Burned,
+            Cardiac,
+            Concussed,
+            Corroding,
+            Deafened,
+            Decontaminated,
+            Disabled,
+            Ensnared,
+            Exhausted,
+            Flashed,
+            Hemorrhage,
+            Poisoned,
+            SeveredHands,
+            Sinkhole,
+            Stained,
+            Traumatized,
+            Suffer
+        };
     }
 }
--- Explode/Explode/EventHandlers.cs
+++ Explode/Explode/EventHandlers.cs
@@ -9,22 +9,190 @@
     using InventorySystem;
     using InventorySystem.Items;
     using InventorySystem.Items.ThrowableProjectiles;
+    using CustomPlayerEffects;
+    using System;
 
     public class EventHandlers
     {
         [PluginEvent(ServerEventType.PlayerPreCoinFlip)]
-        public bool OnPlayerPreCoinFlip(Player player)
+        public void OnPlayerPreCoinFlip(Player player)
         {
             float randNext = (float)Plugin.Random.NextDouble();
             Log.Info(randNext.ToString());
-            if (randNext <= Plugin.Instance.Config.explodeChance)
+            if (randNext <= Plugin.Instance.Config.ExplodeChance)
             {
                 InventoryItemLoader.AvailableItems.TryGetValue(ItemType.GrenadeHE, out ItemBase itemBase);
                 ThrowableItem throwableItem = (ThrowableItem)itemBase;
                 ExplosionGrenade.Explode(new Footprint(player.ReferenceHub), player.Position, (ExplosionGrenade)throwableItem.Projectile);
-                return false;
             }
-            return true;
+        }
+        [PluginEvent(ServerEventType.PlayerCoinFlip)]
+        public void OnPlayerCoinFlip(Player player, bool isTails)
+        {
+            Log.Info(isTails.ToString());
+            // On Heads, give a positive bonus, on Tails, give a negative effect
+            if (!isTails && Plugin.Instance.Config.HasEffects)
+            {
+                // Gets a random positive effect
+                int maxGood = (int)Math.Ceiling(Plugin.Instance.Config.NumGoodEffects * 10* Plugin.Instance.Config.PercentExtremes);
+                maxGood += Plugin.Instance.Config.NumGoodEffects * 10;
+                ApplyEffect(player, Plugin.Random.Next(0, maxGood));
+            }
+            else if(Plugin.Instance.Config.HasEffects)
+            {
+                // Gets a random negative effect
+                int maxBad = (int)Math.Ceiling(Plugin.Instance.Config.NumBadEffects * 10 * Plugin.Instance.Config.PercentExtremes);
+                maxBad += Plugin.Instance.Config.NumBadEffects * 10 + Plugin.Instance.Config.NumGoodEffects+ 10;
+                ApplyEffect(player, Plugin.Random.Next(Plugin.Instance.Config.NumGoodEffects * 10+10, maxBad));
+            }
+            // Otherwise, do nothing
+
+            // Removes the coin if loseCoin is set to true in config
+            if(Plugin.Instance.Config.LoseCoin)
+            {
+                player.RemoveItem(new Item(player.CurrentItem));
+            }
+        }
+        private void ApplyEffect(Player player, int effect)
+        {
+            Log.Info(effect.ToString());
+            // Rounds the effect back down to the enum value
+            int value = (int)Math.Floor(effect / 10.0f);
+            Log.Info(value.ToString());
+
+            // Applies the effect to the player
+            switch(value)
+            {
+                case (int)Config.CoinEffects.BodyshotReduction:
+                    player.EffectsManager.EnableEffect<BodyshotReduction>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.DamageReduction:
+                    player.EffectsManager.EnableEffect<DamageReduction>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Invigorated:
+                    player.EffectsManager.EnableEffect<Invigorated>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Invisibility:
+                    player.EffectsManager.EnableEffect<Invisible>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.MovementBoost:
+                    player.EffectsManager.EnableEffect<MovementBoost>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.SCP1853:
+                    player.EffectsManager.EnableEffect<Scp1853>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.SpawnProtection:
+                    player.EffectsManager.EnableEffect<SpawnProtected>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Rainbow:
+                    player.EffectsManager.EnableEffect<RainbowTaste>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Vitality:
+                    player.EffectsManager.EnableEffect<Vitality>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.SCP207:
+                    player.EffectsManager.EnableEffect<Scp207>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.HotDamn:
+                    player.EffectsManager.EnableEffect<BodyshotReduction>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<DamageReduction>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Invigorated>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Invisible>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<MovementBoost>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Scp1853>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<SpawnProtected>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<RainbowTaste>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Vitality>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Scp207>(Plugin.Instance.Config.Duration);
+                    break;
+                // Negative effects begin here
+                case (int)Config.CoinEffects.Amnesia:
+                    player.EffectsManager.EnableEffect<AmnesiaVision>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<AmnesiaItems>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Asphyxiated:
+                    player.EffectsManager.EnableEffect<Asphyxiated>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Bleeding:
+                    player.EffectsManager.EnableEffect<Bleeding>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Blinded:
+                    player.EffectsManager.EnableEffect<Blinded>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Burned:
+                    player.EffectsManager.EnableEffect<Burned>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Cardiac:
+                    player.EffectsManager.EnableEffect<CardiacArrest>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Concussed:
+                    player.EffectsManager.EnableEffect<Concussed>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Corroding:
+                    player.EffectsManager.EnableEffect<Corroding>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Deafened:
+                    player.EffectsManager.EnableEffect<Deafened>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Decontaminated:
+                    player.EffectsManager.EnableEffect<Decontaminating>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Disabled:
+                    player.EffectsManager.EnableEffect<Disabled>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Ensnared:
+                    player.EffectsManager.EnableEffect<Ensnared>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Exhausted:
+                    player.EffectsManager.EnableEffect<Exhausted>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Flashed:
+                    player.EffectsManager.EnableEffect<Flashed>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Hemorrhage:
+                    player.EffectsManager.EnableEffect<Hemorrhage>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Poisoned:
+                    player.EffectsManager.EnableEffect<Poisoned>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.SeveredHands:
+                    player.EffectsManager.EnableEffect<SeveredHands>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Sinkhole:
+                    player.EffectsManager.EnableEffect<Sinkhole>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Stained:
+                    player.EffectsManager.EnableEffect<Stained>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Traumatized:
+                    player.EffectsManager.EnableEffect<Traumatized>(Plugin.Instance.Config.Duration);
+                    break;
+                case (int)Config.CoinEffects.Suffer:
+                    player.EffectsManager.EnableEffect<AmnesiaVision>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<AmnesiaItems>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Asphyxiated>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Bleeding>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Blinded>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Burned>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<CardiacArrest>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Concussed>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Corroding>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Deafened>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Decontaminating>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Disabled>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Ensnared>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Exhausted>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Flashed>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Hemorrhage>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Poisoned>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<SeveredHands>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Sinkhole>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Stained>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Traumatized>(Plugin.Instance.Config.Duration);
+                    player.EffectsManager.EnableEffect<Decontaminating>(Plugin.Instance.Config.Duration);
+                    break;
+
+            }
         }
     }
 }
